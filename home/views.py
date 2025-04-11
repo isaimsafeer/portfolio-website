d89eb651acca161140828778caf5228ea2aa2891
isaimsafeer <isaimsafeer@gmail.com>
2025-04-11 12:00:48 +0500

    Projects Added

--- home/views.py
+++ home/views.py
@@ -13,7 +13,64 @@ def services(request):
     return render(request, 'services.html')
 
 def projects(request):
-    return render(request, 'projects.html')
+    project_list = [
+        {
+            "title": "Movie Recommender System",
+            "description": "A movie recommendation system based on content filtering, applied data preprocessing, and machine learning.",
+            "technologies": ["Python", "SK-Learn", "Streamlit"],
+            "SourceCode": "https://github.com/isaimsafeer/movie_recomendation",
+            "image": "https://blog.lancedb.com/content/images/size/w1000/2024/05/1_AatBvnpVpEPoQvZAMeqU-A.webp"
+        },
+        {
+            "title": "RAG based Medical Chatbot",
+            "description": "A RAG based Chatbot for Medical help, using RAG and Langchain for medical responses.",
+            "technologies": ["Python", "OpenAI", "Langchain", "Pinecone", "FASTAPI"],
+            "SourceCode": "https://github.com/isaimsafeer/medical_chatbot",
+            "image": "https://kms-healthcare.com/wp-content/uploads/2023/10/ai-chatbots-in-healthcare-1-1024x575.png"
+        },
+        {
+            "title": "Brain Tumor Detection",
+            "description": "A Brain Tumor Detection system using CNN and Keras.",
+            "technologies": ["Python", "Tensorflow", "Keras", "FASTAPI", "HTML", "CSS", "JavaScript"],
+            "SourceCode": "https://github.com/isaimsafeer",
+            "image": "https://media.licdn.com/dms/image/v2/C4D12AQEWHzMrlFh0nQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1651474032780?e=2147483647&v=beta&t=oXw2GMCCQFQ82ldQaVEWNzhh7oMJMoZLYPYkRZjN5KM"
+        }
+    ]
+
+    # Icon map for technologies
+    icon_map = {
+        "Python": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+        "SK-Learn": "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+        "Streamlit": "https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png",
+        "OpenAI": "https://www.svgrepo.com/show/306500/openai.svg",
+        "Langchain": "https://avatars.githubusercontent.com/u/139914090?s=200&v=4",
+        "Pinecone": "https://avatars.githubusercontent.com/u/74971167?s=200&v=4",
+        "FASTAPI": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png",
+        "Tensorflow": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg",
+        "Keras": "https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg",
+        "HTML": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        "CSS": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        "JavaScript": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    }
+
+    # Enrich project data
+    enriched_projects = []
+    for project in project_list:
+        # Add flag for external image
+        project["is_external_image"] = project["image"].startswith("http")
+
+        # Add tech details with icons
+        project["tech_details"] = []
+        for tech in project["technologies"]:
+            project["tech_details"].append({
+                "name": tech,
+                "icon": icon_map.get(tech)
+            })
+
+        enriched_projects.append(project)
+
+    return render(request, 'projects.html', {'projects': enriched_projects})
+
 
 def contact(request):
     return render(request, 'contact.html')
@@ -22,4 +79,7 @@ def contact(request):
 @csrf_exempt
 def ask_chatbot(request):
     return render(request, 'chatbot.html')
+
+
+
         
